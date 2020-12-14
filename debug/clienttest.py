9f9b390e97e71edec9383cc6f0751b10b017f7d8
Gabrielle Cruz <47248597+JhandarTheUnhallowed@users.noreply.github.com>
2020-12-14 09:04:26 +0100

    Phase 6 Implementation 2
    - testing the addition and removal of clients from the list

--- debug/clienttest.py
+++ debug/clienttest.py
@@ -2,8 +2,32 @@ from clients import clientmenu as cm
 from clients import client as c
 import unittest as test
 
+
 class TestAddClient(test.TestCase):
-    def test_add_client_success:
+    def setUp(self):
+        super(TestAddClient, self).setUp()
+        self.addTypeEqualityFunc(str, self.assertMultiLineEqual)
+        
+    def test_add_client_success(self):
+        c.client_list = []
+        cm.add_client('Gabe', 'Cruz', 'test@gmail.com', '0487362360')
+        actual = c.client_list[0].full_info()
+        expected = 'Gabe Cruz, test@gmail.com, 0487362360'
+        self.assertEqual(actual, expected)
+
+        with self.assertRaises(Exception):
+            expected2 = 'Test One, test.com, 0004'
+            self.assertEqual(actual, expected2)
+
+
+class TestRemoveClient(test.TestCase):
+    def setUp(self):
+        super(TestRemoveClient, self).setUp()
+        self.addTypeEqualityFunc(str, self.assertMultiLineEqual)
+
+    def test_remove_client_success(self):
+        c.client_list = []
+
 
 
 if __name__ == "__main__":
