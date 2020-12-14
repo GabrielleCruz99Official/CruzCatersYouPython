ee0d4ee92658487e20da73945a5ac19a4222e771
Gabrielle Cruz <47248597+JhandarTheUnhallowed@users.noreply.github.com>
2020-12-14 08:02:52 +0100

    Phase 6 Implementation 1
    -Testing the Dish Ideas module if we can add or remove dishes from the list

--- debug/functiontest.py
+++ debug/functiontest.py
@@ -1,5 +1,34 @@
-import unittest
+import unittest as test
+from debug import exceptions as exc
+from food import items as it
+
 """
 Here, we will find the application quality control tests
 in case we encounter bugs
 """
+
+
+class TestAddDishToIdea(test.TestCase):
+    def test_add_dish_success(self):
+        it.menu_idea = {}
+        it.add_item('TST', 'Test', 10)
+        actual = it.menu_idea
+        expected = {'TST': {'name': 'Test', 'price': 10}}
+        self.assertDictEqual(actual, expected)
+
+    def test_bad_id(self):
+        with self.assertRaises(exc.IDError):
+            it.add_item('TSTAN', 'Testan', 10)
+
+
+class TestRemoveDish(test.TestCase):
+    def test_remove_dish_success(self):
+        it.menu_idea = {'TSA': {'name': 'TestOne', 'price': 1}, 'TSB': {'name': 'TestTwo', 'price': 2}}
+        it.remove_item('TSB')
+        actual = it.menu_idea
+        expected = {'TSA': {'name': 'TestOne', 'price': 1}}
+        self.assertDictEqual(actual, expected)
+
+
+if __name__ == "__main__":
+    test.main()
